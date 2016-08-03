9acc4de92ca589d513ba9cadd0169190ede19562
Marlie Cross <crossm2@seattleu.edu>
2016-08-03 14:42:16 -0700

    Built magicmissionapp from commit 7fc5a9e on branch master

--- app/scripts/angularfire/config.js
+++
@@ -1,5 +0,0 @@
-angular.module('firebase.config', [])
-  .constant('FBURL', 'https://magic-muscle-mission.firebaseio.com')
-  .constant('SIMPLE_LOGIN_PROVIDERS', ['password','anonymous','facebook','google','twitter'])
-
-  .constant('loginRedirectPath', '/login');
