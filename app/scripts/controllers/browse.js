582741db719d03794c6c58fab879b67c121ceeb0
Maggie van Winkle <vanwinkslist@gmail.com>
2016-08-07 14:27:09 -0700

    Added a browse view. Added MDL card sample.Deleted some old CSS.

---
+++ app/scripts/controllers/browse.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name magicMissionAppApp.controller:BrowseCtrl
+ * @description
+ * # BrowseCtrl
+ * Controller of the magicMissionAppApp
+ */
+angular.module('magicMissionAppApp')
+  .controller('BrowseCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
