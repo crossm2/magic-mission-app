34e91fd7a71b29773249b3f544297e5e1598f981
Maggie van Winkle <vanwinkslist@gmail.com>
2016-08-07 15:50:13 -0700

    Created About page and explored different ways to show data.

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name magicMissionAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the magicMissionAppApp
+ */
+angular.module('magicMissionAppApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
