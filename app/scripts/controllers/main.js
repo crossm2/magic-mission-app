8c88a48c800f8d817d53b6eb49e1c8e2101b74da
Maggie van Winkle <vanwinkslist@gmail.com>
2016-08-19 16:18:33 -0700

    Added gif for demo image. Adjusted styling for exercise details. Messed with JS for creating WOD from DB but didn't get far.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -19,6 +19,39 @@
     // because the request to the server has not returned when we reach this line.
 
 
+    /**
+     * Playing with data
+     * Create a WOD based on Firebase data
+     */
+    //Arrays to hold exercise objects
+    var coreWorkout = [];
+    var upperWorkout = [];
+    var lowerWorkout = [];
+
+  //loop through db
+  //   for (var i=0; i<30; i++) {
+  //     //search for 'core'
+  //     // $scope.data.Exercises.indexOf('Core');
+  //     //push to coreWorkout
+  //
+  //     if ($scope.data.Exercises[i].Section.indexOf('Core')) {
+  //       console.log(coreWorkout);
+  //       // coreWorkout.push($scope.data.Exercises[i]);
+  //     }
+  //   }
+
+    // console.log(coreWorkout);
+
+    // pull random objects from exercise array with Section: Core
+    // Loop through DB for length of DB, looking for Section Core
+    // Add each object to Array Workout
+
+
+
+
+    /**
+     * Setting underline under active Circuit
+     */
     //Set visibility for Circuits
     $scope.showCircuit1 = true;
     $scope.showCircuit2 = false;
@@ -27,7 +60,6 @@
     $scope.circuit2Underline = {
       'background-color': 'transparent'
     };
-
 
     $scope.circuit1= function() {
       //when button for circuit1 is clicked, hide cir2, show cir1
@@ -68,7 +100,7 @@
 
 
     /**
-     * Next exercise
+     * TODO Next exercise
      * User can reject an exercise and get a new suggestion
      */
     $scope.next_Exercise = function() {
@@ -79,7 +111,7 @@
     /**
      * See Demo when user clicks Exercise
      */
-    $scope.see_Demo = function() {
+    $scope.seeDemo = function() {
       console.log('clicked workout');
       // change toggle state for img
       $scope.showDemo = !$scope.showDemo;
